4391b2359b6686a65c32cacf37c37149c5b73d83
Eric Yan <eric.yan@sap.com>
2018-07-19 22:07:00 -0700

    carousels

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
-listOfNGOs = {
-    "listOfNGOs": [
+$(document).ready(() => {
+    let car = createCarousel();
+     document.getElementById('carousels').innerHTML = car;
+
+});
+
+function createCarousel() {
+    let build = '';
+    data.data.forEach((obj) => {
+        build += '<h3>' + obj.category + '</h3><div class="carousel" data-flickity=\'{ "groupCells": true }\'>' +
+        createCells(obj) +
+        '</div>';
+    });
+    return build;
+}
+
+function createCells(obj) {
+    let build = '';
+    obj.ngos.forEach((ngo) => {
+        build += '<div class="carousel-cell">' + createCard(ngo) + '</div>';
+    }); 
+    return build;
+}
+
+function createCard(ngo) {
+    return '<div class="card" style="width: 18rem;">' +
+                '<div class="card-body">' +
+                    '<h5 class="card-title">' + ngo.name + '</h5>' +
+                    '<p class="card-text">' + ngo.description + '</p>' +
+                    '<button type="button" class="btn btn-primary" data-toggle="modal" onclick="readmore()" data-target="#exampleModalCenter">Read More</button>' +
+                '</div>' +
+            '</div>';
+}
+
+function readmore() {
+
+}
+
+data = {
+    "data": [
         {
             "category": "Category1",
             "ngos": [
@@ -10,7 +48,20 @@ listOfNGOs = {
                 {
                     "name": "NGO2",
                     "description": "euoruworweuirowup;"
+                },
+                {
+                    "name": "NGO3",
+                    "description": "euoruworweuirowup;"
+                },
+                {
+                    "name": "NGO4",
+                    "description": "euoruworweuirowup;"
+                },
+                {
+                    "name": "NGO5",
+                    "description": "euoruworweuirowup;"
                 }
+                
             ]
         },
         {
@@ -36,70 +87,13 @@ listOfNGOs = {
                 {
                     "name": "NGO6",
                     "description": "euoruworweuirowup;"
+                },
+                {
+                    "name": "NGO6",
+                    "description": "euoruworweuirowup;"
                 }
             ]
         }
 
     ]
 };
-
-$(document).ready(() => {
-
-    // document.getElementById("listOfCategories").innerHTML += '<a class="dropdown-item" id="'+ ngo +'" href="#'+ ngo +'">'+ ngo +'</a>';
-    // document.getElementById("listOfCategories").innerHTML += '<option value="' + category + '">' + category + '</option>';
-    // let carouselsString = '';
-    // listOfNGOs.listOfNGOs.forEach((category) => {
-    //     let ngos = category.ngos;
-    //     console.log(category.category);
-    //
-    //     carouselsString += '<div class="carousel flickity-enabled" data-flickity="{ &quot;groupCells&quot;: true }" tabindex="0">\n' +
-    //         '        <div class="flickity-viewport" style="height: 156px; touch-action: pan-y;">\n' +
-    //         '            <div class="flickity-slider" style="left: 0px; transform: translateX(10.28%);">\n';
-    //
-    //     ngos.forEach((ngo) => {
-    //         console.log(ngo.name);
-    //         carouselsString += '<div class="carousel-cell" aria-selected="true" >\n' +
-    //             '                    <div class="card" style="width: 6rem;">\n' +
-    //             '                        <div class="card-body">\n' +
-    //             '                            <h5 class="card-title">' + ngo.name + '</h5>\n' +
-    //             '                            <p class="card-text">' + ngo.description + '</p>\n' +
-    //             '                            <a href="#" class="btn btn-primary">Show</a>\n' +
-    //             '                        </div>\n' +
-    //             '                    </div>\n' +
-    //             '                </div>';
-    //
-    //     });
-    //
-    //     carouselsString += ' </div>\n' +
-    //         '        </div>\n' +
-    //         '        <button class="flickity-button flickity-prev-next-button previous" type="button" disabled=""\n' +
-    //         '                aria-label="Previous">\n' +
-    //         '            <svg class="flickity-button-icon" viewBox="0 0 100 100">\n' +
-    //         '                <path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z" class="arrow"></path>\n' +
-    //         '            </svg>\n' +
-    //         '        </button>\n' +
-    //         '        <button class="flickity-button flickity-prev-next-button next" type="button" disabled="" aria-label="Next">\n' +
-    //         '            <svg class="flickity-button-icon" viewBox="0 0 100 100">\n' +
-    //         '                <path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z" class="arrow"\n' +
-    //         '                      transform="translate(100, 100) rotate(180) "></path>\n' +
-    //         '            </svg>\n' +
-    //         '        </button>\n' +
-    //         '        <ol class="flickity-page-dots">\n' +
-    //         '            <li class="dot is-selected" aria-label="Page dot 1" aria-current="step"></li>\n' +
-    //         '        </ol>\n' +
-    //         '    </div>';
-    //
-    //
-    // });
-    // document.getElementById("carousels").innerHTML = carouselsString;
-
-
-});
-
-// function onCategorySelected(category) {
-//     document.getElementById("selectedCategory").value = category.value;
-//
-//
-//     alert(document.getElementById("selectedCategory").value);
-// }
-
