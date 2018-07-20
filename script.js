6825d9b6f4e1010b48ac415a48b8554b1123da95
Anurag Panchal <anurag.panchal@sap.com>
2018-07-19 17:10:08 -0700

    Something

---
+++ script.js
@@ -0,0 +1,105 @@
+listOfNGOs = {
+    "listOfNGOs": [
+        {
+            "category": "Category1",
+            "ngos": [
+                {
+                    "name": "NGO1",
+                    "description": "salkfjdslk;fjsad;flj"
+                },
+                {
+                    "name": "NGO2",
+                    "description": "euoruworweuirowup;"
+                }
+            ]
+        },
+        {
+            "category": "Category2",
+            "ngos": [
+                {
+                    "name": "NGO3",
+                    "description": "salkfjdslk;fjsad;flj"
+                },
+                {
+                    "name": "NGO4",
+                    "description": "euoruworweuirowup;"
+                }
+            ]
+        },
+        {
+            "category": "Category3",
+            "ngos": [
+                {
+                    "name": "NGO5",
+                    "description": "salkfjdslk;fjsad;flj"
+                },
+                {
+                    "name": "NGO6",
+                    "description": "euoruworweuirowup;"
+                }
+            ]
+        }
+
+    ]
+};
+
+$(document).ready(() => {
+
+    // document.getElementById("listOfCategories").innerHTML += '<a class="dropdown-item" id="'+ ngo +'" href="#'+ ngo +'">'+ ngo +'</a>';
+    // document.getElementById("listOfCategories").innerHTML += '<option value="' + category + '">' + category + '</option>';
+    // let carouselsString = '';
+    // listOfNGOs.listOfNGOs.forEach((category) => {
+    //     let ngos = category.ngos;
+    //     console.log(category.category);
+    //
+    //     carouselsString += '<div class="carousel flickity-enabled" data-flickity="{ &quot;groupCells&quot;: true }" tabindex="0">\n' +
+    //         '        <div class="flickity-viewport" style="height: 156px; touch-action: pan-y;">\n' +
+    //         '            <div class="flickity-slider" style="left: 0px; transform: translateX(10.28%);">\n';
+    //
+    //     ngos.forEach((ngo) => {
+    //         console.log(ngo.name);
+    //         carouselsString += '<div class="carousel-cell" aria-selected="true" >\n' +
+    //             '                    <div class="card" style="width: 6rem;">\n' +
+    //             '                        <div class="card-body">\n' +
+    //             '                            <h5 class="card-title">' + ngo.name + '</h5>\n' +
+    //             '                            <p class="card-text">' + ngo.description + '</p>\n' +
+    //             '                            <a href="#" class="btn btn-primary">Show</a>\n' +
+    //             '                        </div>\n' +
+    //             '                    </div>\n' +
+    //             '                </div>';
+    //
+    //     });
+    //
+    //     carouselsString += ' </div>\n' +
+    //         '        </div>\n' +
+    //         '        <button class="flickity-button flickity-prev-next-button previous" type="button" disabled=""\n' +
+    //         '                aria-label="Previous">\n' +
+    //         '            <svg class="flickity-button-icon" viewBox="0 0 100 100">\n' +
+    //         '                <path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z" class="arrow"></path>\n' +
+    //         '            </svg>\n' +
+    //         '        </button>\n' +
+    //         '        <button class="flickity-button flickity-prev-next-button next" type="button" disabled="" aria-label="Next">\n' +
+    //         '            <svg class="flickity-button-icon" viewBox="0 0 100 100">\n' +
+    //         '                <path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z" class="arrow"\n' +
+    //         '                      transform="translate(100, 100) rotate(180) "></path>\n' +
+    //         '            </svg>\n' +
+    //         '        </button>\n' +
+    //         '        <ol class="flickity-page-dots">\n' +
+    //         '            <li class="dot is-selected" aria-label="Page dot 1" aria-current="step"></li>\n' +
+    //         '        </ol>\n' +
+    //         '    </div>';
+    //
+    //
+    // });
+    // document.getElementById("carousels").innerHTML = carouselsString;
+
+
+});
+
+// function onCategorySelected(category) {
+//     document.getElementById("selectedCategory").value = category.value;
+//
+//
+//     alert(document.getElementById("selectedCategory").value);
+// }
+
